06487d15dc18402c9790eca747cb5683e44092ba
Artem <podgory@list.ru>
2020-04-24 13:42:14 +0400

    Добавьте файлы проекта.

---
+++ MathClient/MathClient.cpp
@@ -0,0 +1,24 @@
+﻿// MathClient.cpp : Client app for MathLibrary DLL.
+#include "pch.h" 
+#include <iostream>
+#include "MathLibrary.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int god;
+	cout << "Введите год" << endl;
+	cin >> god;
+
+	visokos_init(god);
+
+	if (visokos_next() == true)
+	{
+		cout << "Високосный";
+	}
+	if (visokos_next() == false)
+	{
+		cout << "Не високосный";
+	}
+}
